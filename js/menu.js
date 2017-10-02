e394428c9c09b0d81eb71fb0d4970eae4f5c1f80
Arkadiusz Lis <vulpes.lis@gmail.com>
2017-10-02 10:42:45 +0200

    Dodany widget facebooka

--- js/menu.js
+++ js/menu.js
@@ -18,6 +18,29 @@ $(function(){
     });
     $(".side-bar").on("click", function(){
         toggle();
-    })
+    });
+    
+    // fb widget
+    var fbwg = $(".fb-widget");
+    var fbwg_button = $(".fb-top-bar");
+    var fbwg_clicked = false;
+    function fbwg_hide(){
+        fbwg.css({
+            "right" : "-"+(fbwg.width()-120)+"px",
+            "bottom" : "-"+(fbwg.height()-42)+"px"
+        });
+    }
+    function fbwg_show(){
+        fbwg.css({
+            "right": "0px",
+            "bottom": "-5px"
+        });
+    }
+    
+    fbwg_hide();
+    window.onresize = fbwg_hide;
+    fbwg.on("mouseenter", fbwg_show);
+    fbwg.on("mouseleave", fbwg_hide);
+    
 });
 
